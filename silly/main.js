bc23516a427976d813e805c6a526deabd34dd923
mncolthom <mncolthom@gmail.com>
2022-10-13 13:32:34 -0600

    Update main.js

--- silly/main.js
+++ silly/main.js
@@ -2,6 +2,8 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+document.querySelector("html").style.backgroundColor = "red";
+
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
