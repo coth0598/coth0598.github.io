925b3aa3dd72c10c592e020b2ddbf4352e5e260f
mncolthom <mncolthom@gmail.com>
2022-10-23 20:48:10 -0600

    update

--- assignments/assignment9/gallery-start/main.js
+++ assignments/assignment9/gallery-start/main.js
@@ -6,7 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const images = ['pic1.1.jpg', `pic2.1.jpg`, `pic3.1.jpg`, `pic4.1.jpg`, `pic5.1.jpg`];
 const alts = {
   'pic1.jpg' : 'Closeup of a human eye',
   'pic2.jpg' : 'Rock that looks like a wave',
