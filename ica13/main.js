0ef428d272b21afd80202b96a40e48955057d880
mncolthom <mncolthom@gmail.com>
2022-11-20 18:40:16 -0600

    update

--- ica13/main.js
+++ ica13/main.js
@@ -12,3 +12,87 @@ const aliceTumbling = [
   const alice1 = document.querySelector("#alice1");
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
+
+  const buttonThen = document.querySelector('.buttonThen');
+  const buttonCallBack = document.querySelector('.buttonCallBack');
+  const buttonPromise = document.querySelector('.buttonPromise');
+  const buttonAsync = document.querySelector('.buttonAsync');
+
+  function tumbleAlice() {
+   
+    
+    const showAnimateObject = alice1.animate(aliceTumbling, aliceTiming);
+
+    let interval = 2001;
+    
+    setTimeout(() => {
+      const showAnimateObject2 = alice2.animate(aliceTumbling, aliceTiming);
+      
+      const interval1 = 2001;
+      
+      setTimeout(() => {
+        const showAnimateObject3 = alice3.animate(aliceTumbling, aliceTiming);
+        
+      }, 2001);
+
+    }, interval);
+  };
+
+// .then() hell version ---------------------------------------------
+function tumbleAliceThen() {
+  const showAnimateObject = alice1.animate(aliceTumbling, aliceTiming);
+  const promise1 = Promise.resolve(showAnimateObject.finished);
+    
+    promise1.then(() => {
+      const showAnimateObject2 = alice2.animate(aliceTumbling, aliceTiming);  
+      const promise2 = Promise.resolve(showAnimateObject2.finished);
+        
+        promise2.then(() => {
+          const showAnimateObject3 = alice3.animate(aliceTumbling, aliceTiming);
+        });
+    });
+
+  
+    
+};
+
+  
+
+// promise version ----------------------------------------------------
+
+function tumbleAliceProm() {
+  
+  const showAnimateObject = alice1.animate(aliceTumbling, aliceTiming);
+
+  const launch = showAnimateObject.finished;
+    
+  launch.then(() => {
+      
+    const showAnimateObject2 = alice2.animate(aliceTumbling, aliceTiming);
+    return showAnimateObject2.finished;
+  })
+
+  .then(() => {
+    const showAnimateObject3 = alice3.animate(aliceTumbling, aliceTiming);
+  });
+
+};
+
+// async version -----------------------------------------------------
+
+async function tumbleAliceAsync() {
+  
+  
+  const showAnimateObject = alice1.animate(aliceTumbling, aliceTiming);
+  await showAnimateObject.finished;
+  const showAnimateObject2 = alice2.animate(aliceTumbling, aliceTiming);
+  await showAnimateObject2.finished;
+  const showAnimateObject3 = alice3.animate(aliceTumbling, aliceTiming);
+   
+};
+
+// button Set ----------------------------------------------------------
+buttonThen.addEventListener('click', tumbleAliceThen);
+buttonCallBack.addEventListener('click', tumbleAlice);
+buttonPromise.addEventListener('click', tumbleAliceProm);
+buttonAsync.addEventListener('click', tumbleAliceAsync);
